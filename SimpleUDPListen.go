609b9f3b14c29e2f01223cb46d3272d2dd9f5412
Isaiah Lee <iclee@usc.edu>
2023-11-29 11:59:28 -0800

    Create SimpleUDPListen.go

---
+++ SimpleUDPListen.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listenIP := "127.0.0.1"
+	listenPort := 12345
+
+	// UDP address creation for listening
+	listenAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", listenIP, listenPort))
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	// UDP connection for listening
+	conn, err := net.ListenUDP("udp", listenAddr)
+	if err != nil {
+		fmt.Println("Error creating UDP listener:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("UDP server listening on", listenAddr)
+
+	// Buffer for incoming messages
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read from the connection
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("Error reading from UDP:", err)
+			return
+		}
+
+		receivedMessage := string(buffer[:n])
+		fmt.Printf("Received message '%s' from %s\n", receivedMessage, addr)
+	}
+}
