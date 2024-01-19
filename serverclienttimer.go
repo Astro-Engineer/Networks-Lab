3c5b1b76109c182c6b8554909d18649e330c61b8
Isaiah Lee <iclee@usc.edu>
2024-01-19 01:11:27 -0800

    leader output

--- serverclienttimer.go
+++ serverclienttimer.go
@@ -373,6 +373,7 @@ func main() {
 						
 					} else if charValue == "V" {
 						setElectionLock(false)
+						fmt.Printf("The leader's priority number is: %d\n", charValue, intValue)
 					}
 				} else {
 					fmt.Println("Invalid input string format")
